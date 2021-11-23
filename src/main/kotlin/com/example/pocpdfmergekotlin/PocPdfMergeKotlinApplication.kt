8b882671079d372dc3833babca71eb4fdcc2d917
Domingos Latorre <domingos.latorre@gmail.com>
2021-11-23 15:32:02 -0300

    first commit

---
+++ src/main/kotlin/com/example/pocpdfmergekotlin/PocPdfMergeKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pocpdfmergekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocPdfMergeKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<PocPdfMergeKotlinApplication>(*args)
+}
